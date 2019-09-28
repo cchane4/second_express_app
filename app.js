b6438937cf0346b3d344f3398a00b86cbfd4cbe0
Chad Chaney <cchane4@gmail.com>
2019-09-28 17:41:28 -0500

    adding a word multiple route

--- app.js
+++ app.js
@@ -15,10 +15,19 @@ app.get("/speak/:animal", (req, res) => {
         dog: "woof"
     }
     let sound = sounds[animal];
-    res.send("The " + req.params.animal + " says " + sound);   
+    res.send("The " + req.params.animal + " says " + sound);
+    
 }); 
-
-
+/* making a route that sends a message number of times specified in the path */ 
+app.get("/repeat/:message/:times", (req, res) => { 
+    let message = req.params.message; 
+    let times = Number(req.params.times); 
+    let result = ""; 
+    for(let i = 0; i < times; i++){
+        result += message + " "; 
+    }
+    res.send(result); 
+});
 
 // if you go to any route not predefined in app.js you get a 404 ERROR MESSAGE.
 app.get ("*", (req, res) => { 
